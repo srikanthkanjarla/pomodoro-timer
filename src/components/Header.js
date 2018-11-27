75ba0f80074f8004aec7cb92627730d71c1be845
srikanthkanjarla <srikanthkanjarla@gmail.com>
2018-11-27 12:22:03 +0530

    Add site header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Pomodoro</h1>
+    </div>
+  );
+}
+export default Header;
