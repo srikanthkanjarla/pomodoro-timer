ccc7dcc16d3f0fa779cb89b63f3b674e1e0095f0
srikanthkanjarla <srikanthkanjarla@gmail.com>
2018-12-04 16:29:33 +0530

    Add quotes component

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './Quotes.css';
 
-function Quotes() {
-  return <h1>hello</h1>;
+function Quotes(props) {
+  const { isBreakTime, quoteText } = props;
+  const quoteStyle = {
+    color: '#3066be',
+  };
+
+  return (
+    <div className="quote" style={isBreakTime ? quoteStyle : {}}>
+      <p>{isBreakTime ? "It's ok to take a break" : quoteText}</p>
+    </div>
+  );
 }
+Quotes.propTypes = {
+  isBreakTime: PropTypes.bool.isRequired,
+  quoteText: PropTypes.string.isRequired,
+};
 export default Quotes;
