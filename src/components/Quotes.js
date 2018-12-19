fa1cdbaabd0761e44cca5ffe171eb8544ddbf35e
srikanthkanjarla <srikanthkanjarla@gmail.com>
2018-12-19 23:20:06 +0530

    Add quoteAuthor info to quote

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -3,19 +3,27 @@ import PropTypes from 'prop-types';
 import './Quotes.css';
 
 function Quotes(props) {
-  const { isBreakTime, quoteText } = props;
+  const { isBreakTime, quoteText, quoteAuthor } = props;
   const quoteStyle = {
     color: '#3066be',
   };
 
   return (
     <div className="quote" style={isBreakTime ? quoteStyle : {}}>
-      {isBreakTime ? <h2>It&apos;s ok to take a break</h2> : <p>{quoteText}</p>}
+      {isBreakTime ? (
+        <h2>It&apos;s ok to take a break</h2>
+      ) : (
+        <div>
+          <p>{quoteText}</p>
+          <span style={{ color: '#a0099f', fontWeight: 'bold' }}>{quoteAuthor}</span>
+        </div>
+      )}
     </div>
   );
 }
 Quotes.propTypes = {
   isBreakTime: PropTypes.bool.isRequired,
   quoteText: PropTypes.string.isRequired,
+  quoteAuthor: PropTypes.string.isRequired,
 };
 export default Quotes;
