ef6c08c7a79887e8ad62563129027c328d5305c3
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-09-04 15:22:05 +0530

    Add quotes for break time

---
+++ src/components/Quote/BreakTimeQuotes.js
@@ -0,0 +1,14 @@
+const breakTimeQuotes = [
+  "It's ok to take a break",
+  'Take a coffee break,you deserve it',
+  'Time for a break',
+  'Relax Refresh Recharge',
+  'The best therapy is taking a break from routine',
+  'Coffee time',
+  'Take a Break, it&apos;s Coffee Time',
+  'Relax and Take a Break',
+  'Breaks are the best part of working',
+  'Enjoy Your Break Time',
+];
+
+export default breakTimeQuotes;
